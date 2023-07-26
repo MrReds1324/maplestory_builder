70e003d74eab883c49cc2aef17ffe2b278cff0c6
MrReds1324 <ryanb27@gmail.com>
2023-07-25 22:00:55 -0400

    Added custom extended tooltip to enable onTriggered when hovering the tooltip

--- lib/modules/utilities.dart
+++ lib/modules/utilities.dart
@@ -16,12 +16,14 @@ class MapleTooltip extends StatelessWidget{
   final Widget? child;
   final String? label;
   final List<Widget> tooltipWidgets;
+  final Function? onHoverFunction;
 
   const MapleTooltip(
     {
       required this.tooltipWidgets,
       this.tooltipTitle = '',
       this.label,
+      this.onHoverFunction,
       this.child,
       super.key
     }
@@ -29,7 +31,7 @@ class MapleTooltip extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return Tooltip(
+    return OnHoverTooltip(
       richMessage: WidgetSpan(
         alignment: PlaceholderAlignment.baseline,
         baseline: TextBaseline.alphabetic,
@@ -51,8 +53,52 @@ class MapleTooltip extends StatelessWidget{
         borderRadius: const BorderRadius.all(
           Radius.circular(4)),
       ),
+      onTriggered: onHoverFunction != null ? () {
+        onHoverFunction?.call(context);
+      } : null,
       waitDuration: const Duration(milliseconds: 350),
       child: child ?? Text(label ?? '', textAlign: TextAlign.center,),
     );
+  }
+}
+
+class OnHoverTooltip extends Tooltip {
+
+  const OnHoverTooltip({
+    super.key,
+    super.message,
+    super.richMessage,
+    super.height,
+    super.padding,
+    super.margin,
+    super.verticalOffset,
+    super.preferBelow,
+    super.excludeFromSemantics,
+    super.decoration,
+    super.textStyle,
+    super.textAlign,
+    super.waitDuration,
+    super.showDuration,
+    super.triggerMode,
+    super.enableFeedback,
+    super.onTriggered,
+    super.child,
+  });
+
+  @override
+  OnHoverTooltipState createState() => OnHoverTooltipState();
+
+}
+
+// Extending the std lib Tooltip to call the onTriggered function when tooltip becomes visible on hover
+class OnHoverTooltipState extends TooltipState {
+
+  @override
+  bool ensureTooltipVisible() {
+    var change = super.ensureTooltipVisible();
+    if (change){
+      widget.onTriggered?.call();
+    }
+    return change;
   }
 }
