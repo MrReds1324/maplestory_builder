ecab44e413c26d81f5b8494888adaa57404b5ccb
MrReds1324 <ryanb27@gmail.com>
2023-08-02 21:25:40 -0400

    Updated difference provider to calculate all ap stat differences and build widget to be shown

--- lib/modules/difference_provider.dart
+++ lib/modules/difference_provider.dart
@@ -7,7 +7,7 @@ class DifferenceCalculator with ChangeNotifier {
 
   late CharacterModel mainCharacterModel;
   late CharacterModel diffCharacterModel;
-  String differenceText = '';
+  Widget differenceWidget = const SizedBox.shrink();
 
 
   DifferenceCalculator(CharacterModel characterModel){
@@ -15,11 +15,48 @@ class DifferenceCalculator with ChangeNotifier {
     diffCharacterModel = characterModel.copyWith();
   }
 
-  void subtractApToStat(int apAmount, StatType statType) {
-    diffCharacterModel.apStatsModule.subtractApToStat(apAmount, statType);
-    diffCharacterModel.calculateEverything();
-    differenceText = '-$apAmount STR (${doublePercentFormater.format((diffCharacterModel.apStatsModule.apStr / mainCharacterModel.apStatsModule.apStr) - 1)})';
+  void updateDifferenceText(){
+
+    void createText(List<Text> textList, num newValue, num originalValue, StatType statType) {
+      var difference = newValue - originalValue;
+      if (difference == 0) {
+        return;
+      }
+      textList.add(
+        Text(
+          '${difference < 0 ? "" : "+"}$difference ${formatStatTypeEnumName(statType)} (${difference < 0 ? "" : "+"}${doublePercentFormater.format(calculteDifferencePercentage(newValue, originalValue))})'
+        )
+      );
+    }
+
+    var textList = <Text>[];
+
+    createText(textList, diffCharacterModel.apStatsModule.apStr, mainCharacterModel.apStatsModule.apStr, StatType.str);
+    createText(textList, diffCharacterModel.apStatsModule.apDex, mainCharacterModel.apStatsModule.apDex, StatType.dex);
+    createText(textList, diffCharacterModel.apStatsModule.apInt, mainCharacterModel.apStatsModule.apInt, StatType.int);
+    createText(textList, diffCharacterModel.apStatsModule.apLuk, mainCharacterModel.apStatsModule.apLuk, StatType.luk);
+    createText(textList, diffCharacterModel.apStatsModule.apHP, mainCharacterModel.apStatsModule.apHP, StatType.hp);
+    createText(textList, diffCharacterModel.apStatsModule.apMP, mainCharacterModel.apStatsModule.apMP, StatType.mp);
+
+
+    differenceWidget = Column(children: textList);
     notifyListeners();
+  }
+
+  void subtractApToStat(int apAmount, StatType statType) {
+    var tempApStats = diffCharacterModel.apStatsModule.copyWith();
+    apAmount = diffCharacterModel.apStatsModule.subtractApToStat(apAmount, statType);
+    diffCharacterModel.calculateEverything();
+    updateDifferenceText();
+    diffCharacterModel.apStatsModule = tempApStats;
+  }
+
+  void addApToStat(int apAmount, StatType statType) {
+    var tempApStats = diffCharacterModel.apStatsModule.copyWith();
+    apAmount = diffCharacterModel.apStatsModule.addApToStat(apAmount, statType);
+    diffCharacterModel.calculateEverything();
+    updateDifferenceText();
+    diffCharacterModel.apStatsModule = tempApStats;
   }
 
 }
