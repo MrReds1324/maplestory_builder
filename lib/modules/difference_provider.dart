89805d388277e29ee8086d07cb564434399ff59a
MrReds1324 <ryanb27@gmail.com>
2023-08-30 11:53:27 -0400

    Updated difference provider to return generated widget

--- lib/modules/difference_provider.dart
+++ lib/modules/difference_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maplestory_builder/core/constants.dart';
+import 'package:maplestory_builder/core/items/equips.dart';
 import 'package:maplestory_builder/modules/character_provider.dart';
 import 'package:maplestory_builder/modules/utilities.dart';
 
@@ -18,9 +19,9 @@ class DifferenceCalculator with ChangeNotifier {
     }
   }
 
-  void updateDifferenceText({isEquipEditing=false}){
+  Widget updateDifferenceText({bool isEquipEditing=false, Equip? replacing}){
 
-    void createText(List<Text> textList, num newValue, num originalValue, {StatType? statType, RangeType? rangeType}) {
+    void createText(List<Text> textList, num newValue, num originalValue, {StatType? statType, RangeType? rangeType, bool isPercentage=false}) {
       assert(statType != null || rangeType != null, "Must provide statType or RangeType");
       var difference = newValue - originalValue;
       if (difference == 0) {
@@ -30,7 +31,7 @@ class DifferenceCalculator with ChangeNotifier {
       Text? text;
       if (statType != null){
         text = Text(
-          '${difference < 0 ? "" : "+"}${doubleRoundFormater.format(difference)} ${formatStatTypeEnumName(statType)} (${difference < 0 ? "" : "+"}${doublePercentFormater.format(calculteDifferencePercentage(newValue, originalValue))})',
+          '${difference < 0 ? "" : "+"}${isPercentage ? doublePercentFormater.format(difference) : doubleRoundFormater.format(difference)} ${formatStatTypeEnumName(statType)} (${difference < 0 ? "" : "+"}${doublePercentFormater.format(calculteDifferencePercentage(newValue, originalValue))})',
           style: TextStyle(
             color: difference < 0 ?Colors.redAccent: Colors.greenAccent,
           ),
@@ -52,22 +53,42 @@ class DifferenceCalculator with ChangeNotifier {
 
     var textList = <Text>[];
 
+    if (isEquipEditing){
+      if (replacing == null) {
+        textList.add(
+          const Text(
+            "Equipping: ",
+          )
+        );
+      }
+      else{
+        textList.add(
+          Text(
+            "Replacing ${replacing.name}: "
+          )
+        );
+      }
+    }
+
     createText(textList, diffCharacterModel.upperDamageRange, mainCharacterModel.upperDamageRange, rangeType: RangeType.damageRange);
     createText(textList, diffCharacterModel.upperBossDamangeRange, mainCharacterModel.upperBossDamangeRange, rangeType: RangeType.bossDamageRange);
-    createText(textList, diffCharacterModel.totalHp, mainCharacterModel.totalHp, statType: StatType.hp);
-    createText(textList, diffCharacterModel.totalMp, mainCharacterModel.totalMp, statType: StatType.mp);
     createText(textList, diffCharacterModel.totalStr, mainCharacterModel.totalStr, statType: StatType.str);
     createText(textList, diffCharacterModel.totalDex, mainCharacterModel.totalDex, statType: StatType.dex);
     createText(textList, diffCharacterModel.totalInt, mainCharacterModel.totalInt, statType: StatType.int);
     createText(textList, diffCharacterModel.totalLuk, mainCharacterModel.totalLuk, statType: StatType.luk);
+    createText(textList, diffCharacterModel.totalHp, mainCharacterModel.totalHp, statType: StatType.hp);
+    createText(textList, diffCharacterModel.totalMp, mainCharacterModel.totalMp, statType: StatType.mp);
+    createText(textList, diffCharacterModel.totalAttack, mainCharacterModel.totalAttack, statType: StatType.attack);
+    createText(textList, diffCharacterModel.totalMAttack, mainCharacterModel.totalMAttack, statType: StatType.mattack);
+    createText(textList, diffCharacterModel.totalDefense, mainCharacterModel.totalDefense, statType: StatType.defense);
+    createText(textList, diffCharacterModel.totalIgnoreDefense, mainCharacterModel.totalIgnoreDefense, statType: StatType.ignoreDefense, isPercentage: true);
 
     if (isEquipEditing) {
-      equipDifferenceWidget = Column(children: textList);
+      return Column(children: textList);
     }
-    else {
-      differenceWidget = Column(children: textList);
-    }
+    differenceWidget = Column(children: textList);
     notifyListeners();
+    return const SizedBox.shrink();
   }
 
   void subtractApToStat(int apAmount, StatType statType) {
@@ -86,12 +107,13 @@ class DifferenceCalculator with ChangeNotifier {
     diffCharacterModel.apStatsModule = tempApStats;
   }
 
-  void compareEditingEquip(){
+  Widget compareEditingEquip(){
     var tempEquip = diffCharacterModel.equipModule.hat;
     diffCharacterModel.equipModule.hat = mainCharacterModel.editingEquip;
     diffCharacterModel.calculateEverything();
-    updateDifferenceText(isEquipEditing: true);
+    var widgetReturn = updateDifferenceText(isEquipEditing: true, replacing: tempEquip);
     diffCharacterModel.equipModule.hat = tempEquip;
+    return widgetReturn;
   }
 
 }
