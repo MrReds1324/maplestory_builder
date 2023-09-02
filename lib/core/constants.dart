3e100ae7ecc91f5172f03c200d6d0fd8e0cfdaf0
MrReds1324 <ryanb27@gmail.com>
2023-09-01 22:40:19 -0400

    Updated enum to have a formatted name and description

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -43,57 +43,73 @@ enum EquipType {
 }
 
 enum RangeType {
-  damageRange,
-  bossDamageRange,
-  effectiveDamageRange,
-  effectiveBossDamageRange,
+  damageRange(formattedName: "Damage Range", description: "Your actual damage range, accountig for both Damage and Final Damage. For Wand, Staff, Shining Rod, Fan, Psy-Limiter, or Lucent Guantlet weapons, this is calculated using Magic Attack. For all other weapons, your damage is calculated using Attack Power."),
+  bossDamageRange(formattedName: "Boss Damage Range", description: "Your actual damage range against bosses, accountig for Damage, Boss Damage, and Final Damage. For Wand, Staff, Shining Rod, Fan, Psy-Limiter, or Lucent Guantlet weapons, this is calculated using Magic Attack. For all other weapons, your damage is calculated using Attack Power."),
+  effectiveDamageRange(formattedName: "Effective Damage Range", description: "Your actual damage range, accountig for Damage, Final Damage, Critical Damage, and Critcal Rate. For Wand, Staff, Shining Rod, Fan, Psy-Limiter, or Lucent Guantlet weapons, this is calculated using Magic Attack. For all other weapons, your damage is calculated using Attack Power."),
+  effectiveBossDamageRange(formattedName: "Effective Boss Damage Range", description: "Your actual damage range against bosses, accountig for Damage, Boss Damage, Final Damage, Critical Damage, Critical Rate, Ignore Defense, and Ignore Elemental Resistance. For Wand, Staff, Shining Rod, Fan, Psy-Limiter, or Lucent Guantlet weapons, this is calculated using Magic Attack. For all other weapons, your damage is calculated using Attack Power.");
+
+  const RangeType({
+    required this.formattedName,
+    required this.description,
+  });
+
+  final String formattedName;
+  final String description;
 }
 
 enum StatType {
-  arcaneForce,
-  attack,
-  attackSpeed,
-  bossDamage,
-  buffDuration,
-  critDamage,
-  critRate,
-  damage,
-  damageNormalMobs,
-  defense,
-  dex,
-  exp,
-  finalAttack,
-  finalDamage,
-  finalDex,
-  finalHp,
-  finalInt,
-  finalLuk,
-  finalMAttack,
-  finalMp,
-  finalStr,
-  hp,
-  ignoreDefense,
-  ignoreElementalDefense,
-  int,
-  itemDropRate,
-  jump,
-  knockbackResistance,
-  level,
-  luk,
-  mattack,
-  mesosObtained,
-  mp,
-  sacredPower,
-  speed,
-  starForce,
-  statusResistance,
-  str,
-  strPercentage,
-  dexPercentage,
-  intPercentage,
-  lukPercentage,
-  allStats,
-  allStatsPercentage,
+  arcaneForce(formattedName: "Arcane Force", description: "In Arcane River areas, if you have less Arcane Force than the monsters do, you'll deal less damage to them. When you have more Arcane Force than a monster, you can attack through that monster's shield. Can be increased by obtaining and enhancing Arcane Symbols, and Hyper Stats."),
+  attack(formattedName: "Attack Power", description: "Displays the current character's Attack Power. The final value is determined by the Attack Power sum and Attack Power % increase multiplication. However Pipsqueak Luminous (Equilibrium) from Monster Life and the Attack Power increase per level stat from Inner Ability are excluded from the multiplication."),
+  attackSpeed(formattedName: "Attack Speed", description: "Displays the current character's Attack Speed. Attack Speed +1 is the slowest while +8 is the soft limit and +10 is the hard limit. It increases with the equipped weapon's Attack Speed, skills, consumables, and Inner Ability. However Magician jobs are not affected by the weapon's Attack Speed, innately starting with +4."),
+  bossDamage(formattedName: "Boss Damage", description: "The higher your Boss Damage, the greater your damage against Boss monsters. This value does not affect normal Damage. Determined by the total combined Damage of your skills and equipment. Values that only apply to specific skills are not included in this calculation."),
+  buffDuration(formattedName: "Buff Duration", description: "The higher the Buff Duration, the longer buffs will last. Does not apply to certain skills. Determined bt the total combined Buff Duration value of all your skills and equipment. Values that only apply to the duration of specific skills are not included in this calculation."),
+  critDamage(formattedName: "Critical Damage", description: "The higher your Critical Damage, the more damage your Critical attacks inflict. This value is added to your base Critical Damage multiplier of 20-50%. This value does not affect normal Damage. You can increase this value with skills or items."),
+  critRate(formattedName: "Critical Rate", description: "Your chance of performing a Critical Attack. You can enhance this value with skills or items."),
+  damage(formattedName: "Damage", description: "The Damage multiplier used to calculate your Damage Range. Determined by the total combined Damage of your skills and equipment. Values that only apply to specific skills are not included in this calculation."),
+  damageNormalMobs(formattedName: "Damage to Normal Monsters", description: ""),
+  defense(formattedName: "Defense", description: "The higher your Defense, the less damage you take from enemies. Some enemy attacks can pierce through Defense. Determined by adding the total combined defense of your equipment to your base Defense."),
+  dex(formattedName: "DEX", description: "DEX increases the attack strength of Bowmen and some Pirates"),
+  exp(formattedName: "EXP", description: ""),
+  finalAttack(formattedName: "Flat Attack Power", description: ""),
+  finalDamage(formattedName: "Final Damage", description: "The damage multiplier that increases your total Damage Range. Determined by multiplying the Final Damage of all your skills."),
+  finalDex(formattedName: "Flat DEX", description: ""),
+  finalHp(formattedName: "Flat HP", description: ""),
+  finalInt(formattedName: "Flat INT", description: ""),
+  finalLuk(formattedName: "Flat LUK", description: ""),
+  finalMAttack(formattedName: "Flat Magic Attack", description: ""),
+  finalMp(formattedName: "Flat MP", description: ""),
+  finalStr(formattedName: "Flat STR", description: ""),
+  hp(formattedName: "HP", description: "HP increases the attack strength of Demon Avenger. Max HP cap is 500,000."),
+  ignoreDefense(formattedName: "Ignore Defense", description: "The higher your Ignore Enemy Defense value the better your attacks penetrate enemy defenses. Determined by multiplying the Ignore Defense values of all your skills and equipment. Values that only apply to specific skills are not included in this calculation. Applies up to 100%."),
+  ignoreElementalDefense(formattedName: "Ignore Elemental Defense", description: ""),
+  int(formattedName: "INT", description: "INT increases the attack strength of Magicians"),
+  itemDropRate(formattedName: "Item Drop Rate", description: "The higher the Item Drop Rate, the more likely that items will drop from defeated monsters. All values are summed to determin the final value. Item Drop Rates can increase, in total, up to 400%. Consumable Items, with the exception of the Wealth Acquisition Potion, can only increase the value by up to 100%. Item potential can increase the value by up to 200%. Does not apply to certain items. Effects that only apply to specific items is not included in the calculation."),
+  jump(formattedName: "Jump", description: "How you can jump. Can be increased to a max of 123%. Skills and equipment can be used to increase or decrease your Jump."),
+  knockbackResistance(formattedName: "Knockback Resistance", description: "Your chance to resist being pushed back by monster attacks. You can increase this value with skills."),
+  level(formattedName: "Level", description: ""),
+  luk(formattedName: "LUK", description: "LUK increases the attack strength of Thieves"),
+  mattack(formattedName: "Magic Attack", description: "Displays the current character's Magic Attack. The final value is determined by the Magic Attack sum and the Magic Attack % increase multiplication. However Pipsqueak Luminous (Equilibrium) from Monster Life and the Magic Attack increase per level stat from Inner Ability are excluded from the multiplication."),
+  mesosObtained(formattedName: "Mesos Obtained", description: "The higher the Mesos Obtained, the more Mesos monsters drop when defeated. The final value is determined by multiplying the basic obtainable Mesos by any Wealth Acquisition Potion value and Reboot passive value, then by additionally multiplying the sum of the remaining values. Additive values can combine to increase the value by up to 300%. Consumable items, with the exception of the Wealth Acquisition Potion, can only increase the value by up to 100%. Item potential can increase the value by up to 100%."),
+  mp(formattedName: "MP", description: "Required for many skills. Max MP cap is 500,000."),
+  sacredPower(formattedName: "Sacred Power", description: "In Grandis areas, if you have less Sacred Power than the monsters do, you'll deal less damage to them. When you have more Sacred Power than a monster, you can attack through that monster's shield. Can be increased by obtaining and enhancing Sacred Symbols."),
+  speed(formattedName: "Speed", description: "Your movement speed. Can be increased to a max of 140% normally. Skills and equipment can boost this even further. Can be increased to 190% while riding a mount."),
+  starForce(formattedName: "Star Force", description: "In Star Force areas, if you have less Star Force than the monsters do, you'll deal less damage to them. When you have more Star Force than a monster, you can attack through that monster's shield. Can be increased by equipping items with Star Force enhancement. Doubled for Overall equipment."),
+  statusResistance(formattedName: "Status Resistance", description: "Your Abnormal Status Resistance determines the amout of Abnormal Statuses affect you. Higher values result in shorter durations. You can increase this value with skills or items."),
+  str(formattedName: "STR", description: "STR increases the attack strength of Warriors and some Pirates"),
+  strPercentage(formattedName: "STR", description: ""),
+  dexPercentage(formattedName: "DEX", description: ""),
+  intPercentage(formattedName: "INT", description: ""),
+  lukPercentage(formattedName: "LUK", description: ""),
+  allStats(formattedName: "All Stats", description: ""),
+  allStatsPercentage(formattedName: "All Stats", description: "");
+
+  const StatType({
+    required this.formattedName,
+    required this.description,
+  });
+
+  final String formattedName;
+  final String description;
 }
 
 enum FlameType {
