989c5b665da2700704551a0f3e462ff35d2a4d86
MrReds1324 <ryanb27@gmail.com>
2023-09-16 00:47:04 -0400

    Updated set effect to make use of EquipName enum and to create set effect container

--- lib/core/constants/set_effect_stats.dart
+++ lib/core/constants/set_effect_stats.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:maplestory_builder/core/constants.dart';
+import 'package:maplestory_builder/core/constants/equip_constants.dart';
 import 'package:maplestory_builder/core/items/equips.dart';
+import 'package:maplestory_builder/modules/utilities.dart';
 
 class SetEffect {
   final EquipSet equipSet;
-  final Map<EquipType, Set<int>> requiredEquips;
+  final Map<EquipType, SetEffectSlot> requiredEquips;
   final Map<int, Map<StatType, num>> rawSetEffect;
-  Map<EquipType, Set<int>> equippedEquips;
+  Map<EquipType, Set<EquipName>> equippedEquips;
   int totalSetItems = 0;
   // Will be using this to cache the calculated set bonus later on
   Map<StatType, num>? cachedSetBonus;
@@ -17,6 +20,94 @@ class SetEffect {
     equippedEquips,
     this.totalSetItems = 0,
   }) : equippedEquips = equippedEquips ?? {};
+
+  SetEffect copyWith({
+    Map<EquipType, Set<EquipName>>? equippedEquips,
+    int? totalSetItems,
+  }) {
+    return SetEffect(
+      equipSet: equipSet, 
+      requiredEquips: requiredEquips,
+      rawSetEffect: rawSetEffect,
+      equippedEquips: equippedEquips ?? Map.of(this.equippedEquips),
+      totalSetItems: totalSetItems ?? this.totalSetItems,
+    );
+  }
+
+  Widget createSetEffectContainer({Equip? addingEquip}) {
+    Widget __createRawSetEffectLine(int setEffectCount, Map<StatType, num> rawEffect) {
+      List<Widget> rawEffectChildren = [
+        Text(
+          "$setEffectCount Set Items Equipped",
+          style: const TextStyle(color: equipFlameColor),
+        )
+      ];
+
+      Color? rawEffectColor;
+      // If the equip we are adding is not in the alread equipped equips
+      if (addingEquip != null && !(equippedEquips[addingEquip.equipType]?.contains(addingEquip.equipName) ?? false) && setEffectCount == (totalSetItems + 1)) {
+        rawEffectColor = Colors.greenAccent;
+      }
+      else {
+        rawEffectColor = setEffectCount <= totalSetItems ? null : missingColor;
+      }
+      
+      rawEffect.forEach((key, value) {
+        rawEffectChildren.add(
+          Text(
+            "${key.formattedName}: +${key.isPercentage ? doubleRoundPercentFormater.format(value) : value}",
+            style: TextStyle(color: rawEffectColor),
+          )
+        );
+      });
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: rawEffectChildren
+      );
+    }
+    
+    List<Widget> widgetChildren = [
+      Center(
+        child: Container(
+          padding: const EdgeInsets.only(bottom: 5),
+          child: Text(
+            equipSet.formattedName,
+            style: const TextStyle(color: equipFlameColor),
+          )
+        )
+      )
+    ];
+
+    requiredEquips.forEach((key, value) { 
+      widgetChildren.addAll(value.createSlotContainer(key, equippedEquips[key] ?? {}, addingEquip: addingEquip));
+    });
+
+    widgetChildren.add(
+      const Divider(
+        height: 15,
+        thickness: 1,
+        color: statColor,
+      )
+    );
+
+    rawSetEffect.forEach((key, value) {
+      widgetChildren.add(__createRawSetEffectLine(key, value));
+    });
+
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: statColor),
+        borderRadius: const BorderRadius.all(Radius.circular(10))
+      ),
+      padding: const EdgeInsets.all(5),
+      width: 225,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: widgetChildren
+      )
+    );
+  }
 
   Map<StatType, num> calculateStats({bool recalculateCache = false}) {
     if (!recalculateCache && cachedSetBonus != null) {
@@ -74,20 +165,20 @@ class SetEffect {
       return false;
     }
     // The set effect does contain the equip
-    else if (requiredEquipType.contains(equip.itemId)) {
+    else if (requiredEquipType.contains(equip.equipName)) {
       var equippedEquipType = equippedEquips[equip.equipType];
       // We have not added the equip yet
       if (equippedEquipType == null) {
-        equippedEquips[equip.equipType] = {equip.itemId.toInt()};
+        equippedEquips[equip.equipType] = {equip.equipName};
         totalSetItems += 1;
         return true;
       }
       // The equip is already part of the set
-      else if (equippedEquipType.contains(equip.itemId)) {
+      else if (equippedEquipType.contains(equip.equipName)) {
         return false;
       }
       else {
-        equippedEquips[equip.equipType]!.add(equip.itemId.toInt());
+        equippedEquips[equip.equipType]!.add(equip.equipName);
         totalSetItems += 1;
         return true;
       }
@@ -105,15 +196,15 @@ class SetEffect {
       return false;
     }
     // The set effect does contain the equip
-    else if (requiredEquipType.contains(equip.itemId)) {
+    else if (requiredEquipType.contains(equip.equipName)) {
       var equippedEquipType = equippedEquips[equip.equipType];
       // We have not added the equip yet
       if (equippedEquipType == null) {
         return false;
       }
       // The equip is already part of the set
-      else if (equippedEquipType.contains(equip.itemId)) {
-        equippedEquips[equip.equipType]!.remove(equip.itemId.toInt());
+      else if (equippedEquipType.contains(equip.equipName)) {
+        equippedEquips[equip.equipType]!.remove(equip.equipName);
         totalSetItems -= 1;
         return true;
       }
@@ -124,19 +215,89 @@ class SetEffect {
     }
     return false;
   }
+}
 
-  SetEffect copyWith({
-    Map<EquipType, Set<int>>? equippedEquips,
-    int? totalSetItems,
-  }) {
-      return SetEffect(
-        equipSet: equipSet, 
-        requiredEquips: requiredEquips,
-        rawSetEffect: rawSetEffect,
-        equippedEquips: equippedEquips ?? Map.of(this.equippedEquips),
-        totalSetItems: totalSetItems ?? this.totalSetItems,
+abstract class AbstractSetEffectSlot {
+
+  const AbstractSetEffectSlot();
+
+  bool contains(EquipName equipName);
+  List<Widget> createSlotContainer(EquipType equipType, Set<EquipName> equippedEquips, {Equip? addingEquip});
+}
+
+// Slot that defines a regular
+class SetEffectSlot extends AbstractSetEffectSlot {
+  final Set<EquipName> any;
+
+  const SetEffectSlot({
+    required this.any
+  });
+
+  @override
+  bool contains(EquipName equipName) {
+    return any.contains(equipName);
+  }
+
+  @override
+  List<Widget> createSlotContainer(EquipType equipType, Set<EquipName> equippedEquips, {Equip? addingEquip}) {
+    List<Widget> returnWidgets = [];
+
+    for (EquipName equipName in any) { 
+      Color? textColor;
+      if (addingEquip?.equipName == equipName) {
+        textColor = equippedEquips.contains(equipName) ? starColor : Colors.greenAccent;
+      }
+      else {
+        textColor = equippedEquips.contains(equipName) ? null : missingColor;
+      }
+
+      returnWidgets.add(
+        Row(
+          children: [
+            Text(
+              equipName.formattedName,
+              style: TextStyle(color: textColor),
+            ),
+            const Spacer(),
+            Text(
+              "(${equipType.formattedName})",
+              style: TextStyle(color: textColor),
+            ),
+          ]
+        )
       );
     }
+
+    return returnWidgets;
+  }
+}
+
+// Choose one slot, used for weapons
+class SetEffectSlotChooseOne extends AbstractSetEffectSlot {
+  final Set<EquipName> chooseOne;
+  final String choosingName;
+
+  const SetEffectSlotChooseOne({
+    required this.chooseOne,
+    required this.choosingName
+  });
+
+  @override
+  bool contains(EquipName equipName) {
+    return chooseOne.contains(equipName);
+  }
+
+  @override
+  List<Widget> createSlotContainer(EquipType equipType, Set<EquipName> equippedEquips, {Equip? addingEquip}) {
+    return [
+      Row(
+        children: [
+          Text("Choose one of $choosingName Weapon"),
+          // Spacer(),
+        ]
+      )
+    ];
+  }
 }
 
 enum EquipSet {
@@ -152,10 +313,10 @@ enum EquipSet {
 SetEffect superiorGollux = SetEffect(
   equipSet: EquipSet.superiorGollux, 
   requiredEquips: const {
-    EquipType.ring: {1},
-    EquipType.pendant: {2},
-    EquipType.belt: {3},
-    EquipType.earrings: {4},
+    EquipType.ring: SetEffectSlot(any: {EquipName.superiorGolluxRing}),
+    EquipType.pendant: SetEffectSlot(any: {EquipName.superiorGolluxPendant}),
+    EquipType.belt: SetEffectSlot(any: {EquipName.superiorGolluxBelt}),
+    EquipType.earrings: SetEffectSlot(any: {EquipName.superiorGolluxEarrings}),
   },
   rawSetEffect: const {
     2: {
