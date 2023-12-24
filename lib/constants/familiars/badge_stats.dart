4ec0ef5784046b088175a11e8d86c8f4c2ab6a0c
MrReds1324 <ryanb27@gmail.com>
2023-12-24 04:14:36 -0500

    sorted badges

--- lib/constants/familiars/badge_stats.dart
+++ lib/constants/familiars/badge_stats.dart
@@ -1,53 +1,11 @@
 import 'package:maplestory_builder/constants/constants.dart';
 
 enum BadgeName {
-  starterBadge(
-    formattedName: "Starter Badge",
+  apocalypseBadge(
+    formattedName: "Apocalypse Badge",
     badgeStats: {
-      StatType.hp: 5
-    }
-  ),
-  snowflakeBadge(
-    formattedName: "Snowflake badge",
-    badgeStats: {
-      StatType.hp: 5
-    }
-  ),
-  oppressorBadge(
-    formattedName: "Oppressor Badge",
-    badgeStats: {
-      StatType.hp: 5
-    }
-  ),
-  shadowBadge(
-    formattedName: "Shadow Badge",
-    badgeStats: {
-      StatType.luk: 2
-    }
-  ),
-  magicalBadge(
-    formattedName: "Magical Badge",
-    badgeStats: {
-      StatType.int: 2
-    }
-  ),
-  secretBadge(
-    formattedName: "Secret Badge",
-    badgeStats: {
-      StatType.attack: 1,
-      StatType.defense: 5
-    }
-  ),
-  mesaBadge(
-    formattedName: "Mesa Badge",
-    badgeStats: {
-      StatType.str: 2
-    }
-  ),
-  peacefulBadge(
-    formattedName: "Peaceful Badge",
-    badgeStats: {
-      StatType.dex: 2
+      StatType.attackPercentage: 0.01,
+      StatType.attack: 2
     }
   ),
   beachBadge(
@@ -57,38 +15,11 @@ enum BadgeName {
       StatType.mattackPercentage: 0.01
     }
   ),
-  rebelBadge(
-    formattedName: "Rebel badge",
+  cityBadge(
+    formattedName: "City Badge",
     badgeStats: {
-      StatType.critRate: 0.01
-    }
-  ),
-  mineralBadge(
-    formattedName: "Mineral Badge",
-    badgeStats: {
-      StatType.defense: 5,
-      StatType.ignoreDefense: 0.03
-    }
-  ),
-  sandyBadge(
-    formattedName: "Sandy Badge",
-    badgeStats: {
-      StatType.damage: 0.01,
-      StatType.defense: 5
-    }
-  ),
-  machineBadge(
-    formattedName: "Machine Badge",
-    badgeStats: {
-      StatType.int: 2,
-      StatType.mattack: 1
-    }
-  ),
-  toyBadge(
-    formattedName: "Toy Badge",
-    badgeStats: {
-      StatType.hp: 10,
-      StatType.ignoreDefense: 0.03
+      StatType.attack: 1,
+      StatType.mattack: 2
     }
   ),
   clockworkBadge(
@@ -98,32 +29,31 @@ enum BadgeName {
       StatType.defense: 5
     }
   ),
-  forestBadge(
-    formattedName: "Forest Badge",
+  cloudBadge(
+    formattedName: "Cloud Badge",
     badgeStats: {
-      StatType.hp: 5,
-      StatType.defense: 5
+      StatType.allStats: 1,
+      StatType.ignoreDefense: 0.03
     }
   ),
-  waveBadge(
-    formattedName: "Wave badge",
+  commerciBadge(
+    formattedName: "Commerci Badge",
     badgeStats: {
-      StatType.mattackPercentage: 0.01,
-      StatType.defense: 20
+      StatType.defensePercentage: 0.02,
+      StatType.critRate: 0.01
     }
   ),
-  wingsBadge(
-    formattedName: "Wings Badge",
+  cursedBadge(
+    formattedName: "CursedBadge",
     badgeStats: {
-      StatType.speed: 1,
-      StatType.allStatsPercentage: 0.01
+      StatType.defensePercentage: 0.02,
+      StatType.critRate: 0.01
     }
   ),
-  snowBadge(
-    formattedName: "Snow Badge",
+  darknessBadge(
+    formattedName: "Darkness Badge",
     badgeStats: {
-      StatType.attack: 1,
-      StatType.dex: 1
+      StatType.damage: 0.01
     }
   ),
   depthsBadge(
@@ -133,11 +63,30 @@ enum BadgeName {
       StatType.mattack: 2
     }
   ),
-  cloudBadge(
-    formattedName: "Cloud Badge",
+  dragonBadge(
+    formattedName: "Dragon Badge",
     badgeStats: {
-      StatType.allStats: 1,
+      StatType.hp: 5,
       StatType.ignoreDefense: 0.03
+    }
+  ),
+  exorcismBadge(
+    formattedName: "Exorcism Badge",
+    badgeStats: {
+      StatType.allStatsPercentage: 0.01
+    }
+  ),
+  forestBadge(
+    formattedName: "Forest Badge",
+    badgeStats: {
+      StatType.hp: 5,
+      StatType.defense: 5
+    }
+  ),
+  futureBadge(
+    formattedName: "Future Badge",
+    badgeStats: {
+      StatType.attackMattackPercentage: 0.01
     }
   ),
   hatchingBadge(
@@ -146,10 +95,116 @@ enum BadgeName {
       StatType.skillLevel: 1,
     }
   ),
-  timeBadge(
-    formattedName: "Time Badge",
+  labyrinthBadge(
+    formattedName: "Labyrinth Badge",
     badgeStats: {
+      StatType.allStatsPercentage: 0.01,
+      StatType.damage: 0.01
+    }
+  ),
+  lionBadge(
+    formattedName: "Lion Badge",
+    badgeStats: {
+      StatType.attackPercentage: 0.01,
+      StatType.hpPercentage: 0.01
+    }
+  ),
+  machineBadge(
+    formattedName: "Machine Badge",
+    badgeStats: {
+      StatType.int: 2,
+      StatType.mattack: 1
+    }
+  ),
+  magicalBadge(
+    formattedName: "Magical Badge",
+    badgeStats: {
+      StatType.int: 2
+    }
+  ),
+  marinerbadge(
+    formattedName: "Mariner Badge",
+    badgeStats: {
+      StatType.jump: 1,
+      StatType.luk: 2,
+    }
+  ),
+  mesaBadge(
+    formattedName: "Mesa Badge",
+    badgeStats: {
+      StatType.str: 2
+    }
+  ),
+  mightyBadge(
+    formattedName: "Mighty Badge",
+    badgeStats: {
+      StatType.str: 2,
+      StatType.attack: 2
+    }
+  ),
+  mineralBadge(
+    formattedName: "Mineral Badge",
+    badgeStats: {
+      StatType.defense: 5,
       StatType.ignoreDefense: 0.03
+    }
+  ),
+  mushkingBadge(
+    formattedName: "Mushking Badge",
+    badgeStats: {
+      StatType.attackMattack: 2
+    }
+  ),
+  oceanBadge(
+    formattedName: "Ocean Badge",
+    badgeStats: {
+      StatType.critRate: 0.01,
+      StatType.damage: 0.01
+    }
+  ),
+  oppressorBadge(
+    formattedName: "Oppressor Badge",
+    badgeStats: {
+      StatType.hp: 5
+    }
+  ),
+  peacefulBadge(
+    formattedName: "Peaceful Badge",
+    badgeStats: {
+      StatType.dex: 2
+    }
+  ),
+  rebelBadge(
+    formattedName: "Rebel badge",
+    badgeStats: {
+      StatType.critRate: 0.01
+    }
+  ),
+  sandyBadge(
+    formattedName: "Sandy Badge",
+    badgeStats: {
+      StatType.damage: 0.01,
+      StatType.defense: 5
+    }
+  ),
+  secretBadge(
+    formattedName: "Secret Badge",
+    badgeStats: {
+      StatType.attack: 1,
+      StatType.defense: 5
+    }
+  ),
+  shadowBadge(
+    formattedName: "Shadow Badge",
+    badgeStats: {
+      StatType.luk: 2
+    }
+  ),
+  showabadge(
+    formattedName: "Showa Badge",
+    badgeStats: {
+      StatType.allStats: 1,
+      StatType.damage: 0.01
     }
   ),
   shrineBadge(
@@ -165,90 +220,49 @@ enum BadgeName {
       StatType.damage: 0.01
     }
   ),
-  showabadge(
-    formattedName: "Showa Badge",
-    badgeStats: {
-      StatType.allStats: 1,
-      StatType.damage: 0.01
-    }
-  ),
-  cityBadge(
-    formattedName: "City Badge",
+  snowBadge(
+    formattedName: "Snow Badge",
     badgeStats: {
       StatType.attack: 1,
-      StatType.mattack: 2
+      StatType.dex: 1
     }
   ),
-  mightyBadge(
-    formattedName: "Mighty Badge",
+  snowflakeBadge(
+    formattedName: "Snowflake badge",
     badgeStats: {
-      StatType.str: 2,
+      StatType.hp: 5
+    }
+  ),
+  starterBadge(
+    formattedName: "Starter Badge",
+    badgeStats: {
+      StatType.hp: 5
+    }
+  ),
+  swampbadge(
+    formattedName: "Swamp badge",
+    badgeStats: {
+      StatType.allStatsPercentage: 0.01,
       StatType.attack: 2
     }
   ),
-  exorcismBadge(
-    formattedName: "Exorcism Badge",
+  timeBadge(
+    formattedName: "Time Badge",
     badgeStats: {
-      StatType.allStatsPercentage: 0.01
+      StatType.ignoreDefense: 0.03
+    }
+  ),
+  toyBadge(
+    formattedName: "Toy Badge",
+    badgeStats: {
+      StatType.hp: 10,
+      StatType.ignoreDefense: 0.03
     }
   ),
   tyrantBadge(
     formattedName: "Tyrant Badge",
     badgeStats: {
       StatType.allStatsPercentage: 0.01
-    }
-  ),
-  lionBadge(
-    formattedName: "Lion Badge",
-    badgeStats: {
-      StatType.attackPercentage: 0.01,
-      StatType.hpPercentage: 0.01
-    }
-  ),
-  futureBadge(
-    formattedName: "Future Badge",
-    badgeStats: {
-      StatType.attackMattackPercentage: 0.01
-    }
-  ),
-  marinerbadge(
-    formattedName: "Mariner Badge",
-    badgeStats: {
-      StatType.jump: 1,
-      StatType.luk: 2,
-    }
-  ),
-  cursedBadge(
-    formattedName: "CursedBadge",
-    badgeStats: {
-      StatType.defensePercentage: 0.02,
-      StatType.critRate: 0.01
-    }
-  ),
-  commerciBadge(
-    formattedName: "Commerci Badge",
-    badgeStats: {
-      StatType.defensePercentage: 0.02,
-      StatType.critRate: 0.01
-    }
-  ),
-  mushkingBadge(
-    formattedName: "Mushking Badge",
-    badgeStats: {
-      StatType.attackMattack: 2
-    }
-  ),
-  darknessBadge(
-    formattedName: "Darkness Badge",
-    badgeStats: {
-      StatType.damage: 0.01
-    }
-  ),
-  dragonBadge(
-    formattedName: "Dragon Badge",
-    badgeStats: {
-      StatType.hp: 5,
-      StatType.ignoreDefense: 0.03
     }
   ),
   voidbadge(
@@ -258,32 +272,18 @@ enum BadgeName {
       StatType.mattack: 2
     }
   ),
-  labyrinthBadge(
-    formattedName: "Labyrinth Badge",
+  waveBadge(
+    formattedName: "Wave badge",
     badgeStats: {
-      StatType.allStatsPercentage: 0.01,
-      StatType.damage: 0.01
+      StatType.mattackPercentage: 0.01,
+      StatType.defense: 20
     }
   ),
-  apocalypseBadge(
-    formattedName: "Apocalypse Badge",
+  wingsBadge(
+    formattedName: "Wings Badge",
     badgeStats: {
-      StatType.attackPercentage: 0.01,
-      StatType.attack: 2
-    }
-  ),
-  oceanBadge(
-    formattedName: "Ocean Badge",
-    badgeStats: {
-      StatType.critRate: 0.01,
-      StatType.damage: 0.01
-    }
-  ),
-  swampbadge(
-    formattedName: "Swamp badge",
-    badgeStats: {
-      StatType.allStatsPercentage: 0.01,
-      StatType.attack: 2
+      StatType.speed: 1,
+      StatType.allStatsPercentage: 0.01
     }
   ),
   ;
