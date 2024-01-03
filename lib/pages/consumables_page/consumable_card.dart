c3bdaca1bd2dda13b53b5311b1cf787caab1a7e8
MrReds1324 <ryanb27@gmail.com>
2024-01-03 05:32:32 -0500

    Fleshed out the UI portion of deactivating certain consumables

--- lib/pages/consumables_page/consumable_card.dart
+++ lib/pages/consumables_page/consumable_card.dart
@@ -30,65 +30,120 @@ class ConsumableCard extends StatelessWidget {
     }
   }
 
+  void Function(bool?)? _getOnChangedFunction(BuildContext context, bool isSelected, bool isActivated) {
+    void onChanged(bool? newValue) {
+      context.read<ConsumablesProvider>().selectConsumable(consumableName, newValue);
+    }
+
+    if (isSelected) {
+      return onChanged;
+    }
+    else if (!isActivated) {
+      return null;
+    }
+
+    return onChanged;
+  }
+
+  bool _getActivatedState(BuildContext context, bool? isActivated) {
+    if (isActivated == null) {
+      if (context.read<ConsumablesProvider>().activeConsumablesSet.selectedConsumables.keys.contains(ConsumableName.advancedBlessingPotion)) {
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
+    else {
+      return isActivated;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       color: DEFAULT_COLOR,
       child: Container(
-        padding: const EdgeInsets.all(5),
-        child: Selector<ConsumablesProvider, (bool, bool?)>(
-          selector: (_, consumablesProvider) => (consumablesProvider.activeConsumablesSet.selectedConsumables[consumableName] != null, consumablesProvider.activeConsumablesSet.selectedConsumables[consumableName]),
+        padding: const EdgeInsets.all(2.5),
+        child: Selector<ConsumablesProvider, (bool, bool)>(
+          selector: (_, consumablesProvider) => (consumablesProvider.activeConsumablesSet.selectedConsumables[consumableName] != null, _getActivatedState(context, consumablesProvider.activeConsumablesSet.selectedConsumables[consumableName])),
           builder: (context, data, child) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      constraints: const BoxConstraints(
-                        maxWidth: 237
+            return Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: data.$2 ? DEFAULT_COLOR: Colors.redAccent),
+                borderRadius: const BorderRadius.all(Radius.circular(10))
+              ),
+              padding: const EdgeInsets.all(2.5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: child ?? const SizedBox.shrink(),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Duration: ${_getDurationString()}"),
+                      const Spacer(),
+                      const Text("Selected"),
+                      const SizedBox(width: 5),
+                      Checkbox(
+                        isError: true,
+                        value: data.$1,
+                        onChanged: _getOnChangedFunction(context, data.$1, data.$2),
                       ),
-                      child: Text(
-                        consumableName.formattedName,
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                    ),
-                    MapleTooltip(
-                      tooltipWidgets: [
-                        Text(consumableName.description),
-                      ],
-                      child: Icon(MdiIcons.informationOutline),
-                    )
-                  ],
-                ),
-                _ConsumableStatsListView(
-                  consumableName: consumableName,
-                ),
-                const Spacer(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Duration: ${_getDurationString()}"),
-                    const Spacer(),
-                    const Text("Selected"),
-                    const SizedBox(width: 5),
-                    Checkbox(
-                      isError: true,
-                      value: data.$1,
-                      onChanged: (bool? newValue) {
-                        context.read<ConsumablesProvider>().selectConsumable(consumableName, newValue);
-                      },
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             );
-          }
+          },
+          child: _ConsumableInnerContents(consumableName: consumableName),
         ),
       ),
+    );
+  }
+}
+
+class _ConsumableInnerContents extends StatelessWidget {
+  final ConsumableName consumableName;
+
+  const _ConsumableInnerContents({
+    required this.consumableName,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              constraints: const BoxConstraints(
+                maxWidth: 235
+              ),
+              child: Text(
+                consumableName.formattedName,
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ),
+            MapleTooltip(
+              tooltipWidgets: [
+                Text(consumableName.description),
+              ],
+              child: Icon(MdiIcons.informationOutline),
+            )
+          ],
+        ),
+        _ConsumableStatsListView(
+          consumableName: consumableName,
+        ),
+        const Spacer(),
+      ]
     );
   }
 }
@@ -104,7 +159,6 @@ class _ConsumableStatsListView extends StatelessWidget {
   Widget build(BuildContext context) {
     var consumableStats = consumableName.statValues.entries.toList();
     return SizedBox(
-      height: 150,
       width: 300,
       child: ListView.builder(
         padding: const EdgeInsets.only(right: 8),
